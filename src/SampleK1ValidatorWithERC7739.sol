7726c6256ed265de3b4b9348266ed656d4c6edb0
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2024-09-23 15:15:53 +0300

    tests

---
+++ src/SampleK1ValidatorWithERC7739.sol
@@ -0,0 +1,288 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { ERC7739Validator } from "./ERC7739Validator.sol";
+import { SignatureCheckerLib } from "solady/utils/SignatureCheckerLib.sol";
+import { ERC7579ValidatorBase } from "modulekit/Modules.sol";
+import { PackedUserOperation } from "modulekit/external/ERC4337.sol";
+import { EnumerableSet } from "./utils/EnumerableSet4337.sol";
+import { ModuleInstallLib } from "./utils/ModuleInstallLib.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+contract SampleK1ValidatorWithERC7739 is ERC7579ValidatorBase, ERC7739Validator {
+
+    using SignatureCheckerLib for address;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using ModuleInstallLib for bytes;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            CONSTANTS & STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    uint256 constant ERC7579_MODULE_TYPE_STATELESS_VALIDATOR = 7;
+
+    /// @notice Mapping of smart account addresses to their respective owner addresses
+    mapping(address => address) public smartAccountOwners;
+
+    EnumerableSet.AddressSet private safeSenders;
+
+    /// @notice Error to indicate that no owner was provided during installation
+    error NoOwnerProvided();
+
+    /// @notice Error to indicate that the new owner cannot be the zero address
+    error ZeroAddressNotAllowed();
+
+    /// @notice Error to indicate the module is already initialized
+    error ModuleAlreadyInitialized();
+
+    /// @notice Error to indicate that the new owner cannot be a contract address
+    error NewOwnerIsContract();
+
+    /// @notice Error to indicate that the data length is invalid
+    error InvalidDataLength();
+
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     CONFIG
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /**
+     * Initialize the module with the given data
+     *
+     * @param data The data to initialize the module with
+     */
+    function onInstall(bytes calldata data) external override {
+        require(data.length != 0, NoOwnerProvided());
+        require(!_isInitialized(msg.sender), ModuleAlreadyInitialized());
+        if(data.asValidator()) {
+            address newOwner = address(bytes20(data[1:21]));
+            require(!_isContract(newOwner), NewOwnerIsContract());
+            smartAccountOwners[msg.sender] = newOwner;
+            if (data.length > 21) {
+                _fillSafeSenders(data[21:]);
+            }
+        }
+    }
+
+    /**
+     * De-initialize the module with the given data
+     *
+     * @param data The data to de-initialize the module with
+     */
+    function onUninstall(bytes calldata data) external override {
+        if(data.asValidator()) {
+            delete smartAccountOwners[msg.sender];
+            safeSenders.removeAll(msg.sender);
+        }
+    }
+
+    /**
+     * Check if the module is initialized
+     * @param smartAccount The smart account to check
+     *
+     * @return true if the module is initialized, false otherwise
+     */
+    function isInitialized(address smartAccount) external view returns (bool) {
+        return _isInitialized(smartAccount);
+    }
+
+    /// @notice Transfers ownership of the validator to a new owner
+    /// @param newOwner The address of the new owner
+    function transferOwnership(address newOwner) external {
+        require(newOwner != address(0), ZeroAddressNotAllowed());
+        require(!_isContract(newOwner), NewOwnerIsContract());
+        smartAccountOwners[msg.sender] = newOwner;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     MODULE LOGIC
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /**
+     * Validates PackedUserOperation
+     *
+     * @param userOp UserOperation to be validated
+     * @param userOpHash Hash of the UserOperation to be validated
+     *
+     * @return uint256 the result of the signature validation, which can be:
+     *  - 0 if the signature is valid
+     *  - 1 if the signature is invalid
+     *  - <20-byte> aggregatorOrSigFail, <6-byte> validUntil and <6-byte> validAfter (see ERC-4337
+     * for more details)
+     */
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash) external view override returns (ValidationData) {
+        return _validateSignatureForOwner(smartAccountOwners[userOp.sender], userOpHash, userOp.signature) ? VALIDATION_SUCCESS : VALIDATION_FAILED;
+    }
+
+    /**
+     * Validates an ERC-1271 signature
+     *
+     * @param sender The sender of the ERC-1271 call to the account
+     * @param hash The hash of the message
+     * @param signature The signature of the message
+     *
+     * @return sigValidationResult the result of the signature validation, which can be:
+     *  - EIP1271_SUCCESS if the signature is valid
+     *  - EIP1271_FAILED if the signature is invalid
+     */
+    function isValidSignatureWithSender(
+        address sender,
+        bytes32 hash,
+        bytes calldata signature
+    )
+        external
+        view
+        virtual
+        override
+        returns (bytes4 sigValidationResult)
+    {   
+        // can put additional checks based on sender here
+        
+        // check if sig is valid
+        bool success = _erc1271IsValidSignatureWithSender(sender, hash, _erc1271UnwrapSignature(signature));
+        /// @solidity memory-safe-assembly
+        assembly {
+            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
+            // We use `0xffffffff` for invalid, in convention with the reference implementation.
+            sigValidationResult := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
+        } 
+    }
+
+    /// @notice ISessionValidator interface for smart session
+    /// @param hash The hash of the data to validate
+    /// @param sig The signature data
+    /// @param data The data to validate against (owner address in this case)
+    function validateSignatureWithData(bytes32 hash, bytes calldata sig, bytes calldata data) external view returns (bool validSig) {
+        require(data.length == 20, InvalidDataLength());
+        address owner = address(bytes20(data[0:20]));
+        return _validateSignatureForOwner(owner, hash, sig);
+    }
+
+    /// @notice Adds a safe sender to the safeSenders list for the smart account
+    function addSafeSender(address sender) external {
+        safeSenders.add(msg.sender, sender);
+    }
+
+    /// @notice Removes a safe sender from the safeSenders list for the smart account
+    function removeSafeSender(address sender) external {
+        safeSenders.remove(msg.sender, sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     INTERNAL
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Returns whether the `hash` and `signature` are valid.
+    ///      Obtains the authorized signer's credentials and calls some
+    ///      module's specific internal function to validate the signature
+    ///      against credentials.
+    function _erc1271IsValidSignatureNowCalldata(bytes32 hash, bytes calldata signature)
+        internal
+        view
+        override
+        returns (bool) 
+    {
+        // obtain credentials
+        address owner = smartAccountOwners[msg.sender];
+
+        // call custom internal function to validate the signature against credentials
+        return _validateSignatureForOwner(owner, hash, signature);
+    }
+
+    /// @dev Returns whether the `sender` is considered safe, such
+    /// that we don't need to use the nested EIP-712 workflow.
+    /// See: https://mirror.xyz/curiousapple.eth/pFqAdW2LiJ-6S4sg_u1z08k4vK6BCJ33LcyXpnNb8yU
+    // The canonical `MulticallerWithSigner` at 0x000000000000D9ECebf3C23529de49815Dac1c4c
+    // is known to include the account in the hash to be signed.
+    // msg.sender = Smart Account
+    // sender = 1271 og request sender        
+    function _erc1271CallerIsSafe(address sender) internal view virtual override returns (bool) {
+        return ( 
+                    sender == 0x000000000000D9ECebf3C23529de49815Dac1c4c || // MulticallerWithSigner
+                    sender == msg.sender || // Smart Account. Assume smart account never sends non safe eip-712 struct
+                    safeSenders.contains(msg.sender, sender) // check if sender is in safeSenders for the Smart Account
+                );
+    }
+   
+    /// @notice Internal method that does the job of validating the signature via ECDSA (secp256k1)
+    /// @param owner The address of the owner
+    /// @param hash The hash of the data to validate
+    /// @param signature The signature data
+    function _validateSignatureForOwner(address owner, bytes32 hash, bytes calldata signature) internal view returns (bool) {
+        // Check if the 's' value is valid
+        bytes32 s;
+        assembly {
+            // same as `s := mload(add(signature, 0x40))` but for calldata
+            s := calldataload(add(signature.offset, 0x20))
+        }
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            return false;
+        }
+
+        if (SignatureCheckerLib.isValidSignatureNowCalldata(owner, hash, signature)) {
+            return true;
+        }
+        if (SignatureCheckerLib.isValidSignatureNowCalldata(owner, MessageHashUtils.toEthSignedMessageHash(hash), signature)) {
+            return true;
+        }
+        return false;
+    }
+
+    /// @notice Checks if the smart account is initialized with an owner
+    /// @param smartAccount The address of the smart account
+    /// @return True if the smart account has an owner, false otherwise
+    function _isInitialized(address smartAccount) private view returns (bool) {
+        return smartAccountOwners[smartAccount] != address(0);
+    }
+
+    /// @notice Checks if the address is a contract
+    /// @param account The address to check
+    /// @return True if the address is a contract, false otherwise
+    function _isContract(address account) private view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+
+    // @notice Fills the safeSenders list from the given data
+    function _fillSafeSenders(bytes calldata data) private {
+        for (uint256 i; i < data.length / 20; i++) {
+            safeSenders.add(msg.sender, address(bytes20(data[20*i:20*(i+1)])));
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     METADATA
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /**
+     * The name of the module
+     *
+     * @return name The name of the module
+     */
+    function name() external pure returns (string memory) {
+        return "Sample ERC7739 enabled Validator";
+    }
+
+    /**
+     * The version of the module
+     *
+     * @return version The version of the module
+     */
+    function version() external pure returns (string memory) {
+        return "0.0.1";
+    }
+
+    /**
+     * Check if the module is of a certain type
+     *
+     * @param typeId The type ID to check
+     *
+     * @return true if the module is of the given type, false otherwise
+     */
+    function isModuleType(uint256 typeId) external pure override returns (bool) {
+        return typeId == TYPE_VALIDATOR || typeId == ERC7579_MODULE_TYPE_STATELESS_VALIDATOR;
+    }
+}
---
+++ test/SampleK1ValidatorWithERC7739.t.sol
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { Test } from "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import {
+    RhinestoneModuleKit,
+    ModuleKitHelpers,
+    ModuleKitUserOp,
+    AccountInstance,
+    UserOpData
+} from "modulekit/ModuleKit.sol";
+import { MODULE_TYPE_VALIDATOR, MODULE_TYPE_FALLBACK, CALLTYPE_SINGLE} from "modulekit/external/ERC7579.sol";
+import { SampleK1ValidatorWithERC7739 } from "src/SampleK1ValidatorWithERC7739.sol";
+import { EIP5267CompatibilityFallback } from "src/EIP5267CompatibilityFallback.sol";
+import { ModuleInstallLib } from "src/utils/ModuleInstallLib.sol";
+import { EIP712 } from "solady/utils/EIP712.sol";
+
+contract SampleK1ValidatorWithERC7739Test is RhinestoneModuleKit, Test {
+    using ModuleKitHelpers for *;
+    using ModuleKitUserOp for *;
+    using ModuleInstallLib for bytes;
+
+    bytes32 internal constant APP_DOMAIN_SEPARATOR = 0xa1a044077d7677adbbfa892ded5390979b33993e0e2a457e3f974bbcda53821b;
+
+    // account and modules
+    AccountInstance internal instance;
+    SampleK1ValidatorWithERC7739 internal validator;
+    EIP5267CompatibilityFallback internal fallbackModule;
+
+    Account owner;
+
+    // test 1271 validation from regulat sender, from safe sender and RPC call?
+    // https://book.getfoundry.sh/cheatcodes/rpc
+
+    function setUp() public {
+        init();
+
+        // Create the validator
+        validator = new SampleK1ValidatorWithERC7739();
+        vm.label(address(validator), "SampleK1ValidatorWithERC7739");
+
+        fallbackModule = new EIP5267CompatibilityFallback();
+
+        owner = makeAccount("owner");
+
+        // Create the account and install the validator
+        instance = makeAccountInstance("Sample7739Validator");
+        vm.deal(address(instance.account), 10 ether);
+        instance.installModule({
+            moduleTypeId: MODULE_TYPE_VALIDATOR,
+            module: address(validator),
+            data: abi.encodePacked(owner.addr).encodeAsValidatorData()
+        });
+
+        //install compatibility fallback
+        bytes memory _fallback = abi.encode(EIP712.eip712Domain.selector, CALLTYPE_SINGLE, "");
+        instance.installModule({ moduleTypeId: MODULE_TYPE_FALLBACK, module: address(fallbackModule), data: _fallback });
+    }
+
+    /// @notice Tests the validation of a personal signature
+    function test_isValidSignature_ERC7739_PersonalSign_Success() public {
+        TestTemps memory t;
+        t.contents = keccak256("123");
+        bytes32 hashToSign = toERC1271HashPersonalSign(t.contents, instance.account);
+        (t.v, t.r, t.s) = vm.sign(owner.key, hashToSign);
+        bytes memory signature = abi.encodePacked(t.r, t.s, t.v);
+        
+        vm.prank(instance.account);
+        bytes4 res = validator.isValidSignatureWithSender(address(this), t.contents, signature);
+        assertEq(res, bytes4(0x1626ba7e));
+    }
+
+    /// @notice Tests the validation of an EIP-712 typed data signature
+    function test_isValidSignature_ERC7739_TypedData_Success() public {
+        TestTemps memory t;
+        t.contents = keccak256("0x1234");
+        bytes32 dataToSign = toERC1271Hash(t.contents, instance.account);
+        (t.v, t.r, t.s) = vm.sign(owner.key, dataToSign);
+        bytes memory contentsType = "Contents(bytes32 stuff)";
+        bytes memory signature = abi.encodePacked(t.r, t.s, t.v, APP_DOMAIN_SEPARATOR, t.contents, contentsType, uint16(contentsType.length));
+        
+        vm.prank(instance.account);
+        bytes4 res = validator.isValidSignatureWithSender(address(this), toContentsHash(t.contents), signature);
+        assertEq(res, bytes4(0x1626ba7e));
+    }
+
+    /// @notice Tests the validation of an EIP-712 signature via safe sender.
+    function test_isValidSignature_Vanilla1271_TypedData_SafeSender() public {
+        
+        // add safe sender
+
+        TestTemps memory t;
+        t.contents = keccak256("0x1234");
+        bytes32 dataToSign = toERC1271Hash(t.contents, instance.account);
+        (t.v, t.r, t.s) = vm.sign(owner.key, dataToSign);
+        bytes memory contentsType = "Contents(bytes32 stuff)";
+        bytes memory signature = abi.encodePacked(t.r, t.s, t.v, APP_DOMAIN_SEPARATOR, t.contents, contentsType, uint16(contentsType.length));
+        
+        vm.prank(instance.account);
+        bytes4 res = validator.isValidSignatureWithSender(address(this), toContentsHash(t.contents), signature);
+        assertEq(res, bytes4(0x1626ba7e));
+    }
+
+
+    // ===== HELPERS ======
+
+    struct TestTemps {
+        bytes32 userOpHash;
+        bytes32 contents;
+        address signer;
+        uint256 privateKey;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 missingAccountFunds;
+    }
+
+    struct AccountDomainStruct {
+        bytes1 fields;
+        string name;
+        string version;
+        uint256 chainId;
+        address verifyingContract;
+        bytes32 salt;
+        uint256[] extensions;
+    }
+
+
+    /// @notice Generates an ERC-1271 hash for personal sign.
+    /// @param childHash The child hash.
+    /// @return The ERC-1271 hash for personal sign.
+    function toERC1271HashPersonalSign(bytes32 childHash, address account) internal view returns (bytes32) {
+        AccountDomainStruct memory t;
+        (t.fields, t.name, t.version, t.chainId, t.verifyingContract, t.salt, t.extensions) = EIP712(account).eip712Domain();
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(t.name)),
+                keccak256(bytes(t.version)),
+                t.chainId,
+                t.verifyingContract
+            )
+        );
+        bytes32 parentStructHash = keccak256(abi.encode(keccak256("PersonalSign(bytes prefixed)"), childHash));
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, parentStructHash));
+    }
+
+    /// @notice Generates an ERC-1271 hash for the given contents and account.
+    /// @param contents The contents hash.
+    /// @param account The account address.
+    /// @return The ERC-1271 hash.
+    function toERC1271Hash(bytes32 contents, address account) internal view returns (bytes32) {
+        bytes32 parentStructHash = keccak256(
+            abi.encodePacked(
+                abi.encode(
+                    keccak256(
+                        "TypedDataSign(Contents contents,bytes1 fields,string name,string version,uint256 chainId,address verifyingContract,bytes32 salt,uint256[] extensions)Contents(bytes32 stuff)"
+                    ),
+                    contents
+                ),
+                accountDomainStructFields(account)
+            )
+        );
+        return keccak256(abi.encodePacked("\x19\x01", APP_DOMAIN_SEPARATOR, parentStructHash));
+    }
+
+    /// @notice Generates a contents hash.
+    /// @param contents The contents hash.
+    /// @return The EIP-712 hash.
+    function toContentsHash(bytes32 contents) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(hex"1901", APP_DOMAIN_SEPARATOR, contents));
+    }
+
+    /// @notice Retrieves the EIP-712 domain struct fields.
+    /// @param account The account address.
+    /// @return The encoded EIP-712 domain struct fields.
+    function accountDomainStructFields(address account) internal view returns (bytes memory) {
+        AccountDomainStruct memory t;
+        (t.fields, t.name, t.version, t.chainId, t.verifyingContract, t.salt, t.extensions) = EIP712(account).eip712Domain();
+
+        return
+            abi.encode(
+                t.fields,
+                keccak256(bytes(t.name)),
+                keccak256(bytes(t.version)),
+                t.chainId,
+                t.verifyingContract, // Use the account address as the verifying contract.
+                t.salt,
+                keccak256(abi.encodePacked(t.extensions))
+            );
+    }
+}
